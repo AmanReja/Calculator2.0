86b60c309e3f1be81b8143353e46521925eff6d1
Aman Reja <amnmolla742@gmail.com>
2024-11-06 22:54:30 +0530

    fixed lag

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll(".button");
+console.log(display, buttons);
+let string = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log("button was clicked");
+    if (e.target.innerHTML == "=") {
+      try {
+        string = eval(string);
+        display.value = string;
+      } catch (error) {
+        display.value = "Error";
+        console.error(" expression", error);
+      }
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      display.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.slice(0, string.length - 1);
+      display.value = string;
+    } else {
+      string += e.target.innerHTML;
+      display.value = string;
+    }
+    console.log(string);
+  });
+});
